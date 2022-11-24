17b997b1a3d564976c945ec83760a81b902b7bac
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-11-23 22:10:58 -0300

    Teste 01 - Requisito 03: Favorite Pokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -4,14 +4,47 @@ import { FavoritePokemon } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Verifica o comportamento da página Favorite Pokémon', () => {
-  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
-    renderWithRouter(<FavoritePokemon />);
-    const { pokemonList } = FavoritePokemon.props;
-    const range = pokemonList.length;
-    const noFavorites = screen.getByText('No favorite Pokémon found');
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', async () => {
+    const pokemonList = [];
+    renderWithRouter(<FavoritePokemon
+      pokemonList={ pokemonList }
+    />);
+    const noFavorites = await screen.findByText('No favorite Pokémon found');
 
-    expect(range === 0).toBeInTheDocument(noFavorites);
+    expect(noFavorites).toBeInTheDocument();
   });
 
-  it('Teste se são exibidos todos os cards de Pokémon favoritados', () => {});
+  it('Teste se são exibidos todos os cards de Pokémon favoritados', async () => {
+    const pokemonList = [{
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+    }];
+    renderWithRouter(<FavoritePokemon
+      pokemonList={ pokemonList }
+    />);
+    const favorites = await screen.findByRole('img', {
+      name: 'Pikachu sprite',
+      src: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    });
+
+    expect(favorites).toBeInTheDocument();
+  });
 });
